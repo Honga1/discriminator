c762679bca469100da3c77385533b316772d7590
Jae Perris <jae.perris@gmail.com>
2021-01-07 13:37:20 +0100

    Add grommet theme

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -1,10 +1,10 @@
-import { Box, Grommet } from "grommet";
+import { Box, grommet, Grommet } from "grommet";
 import { PropsWithChildren } from "react";
 import { NavMenu } from "./NavMenu";
 
 export const PageContainer = ({ children }: PropsWithChildren<{}>) => {
   return (
-    <Grommet>
+    <Grommet theme={grommet}>
       <Box direction="row">
         <NavMenu />
         <Box>{children}</Box>
