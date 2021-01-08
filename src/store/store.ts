7824c645256bb917ccfdb74305742c8f2c76e664
Jae Perris <jae.perris@gmail.com>
2021-01-08 13:37:09 +0100

    Create store

---
+++ src/store/store.ts
@@ -0,0 +1,34 @@
+import createStoreHook from "zustand";
+import create, { SetState } from "zustand/vanilla";
+import { ProgressNames } from "./Progress";
+
+type State = {
+  progress: ProgressNames;
+  setProgress(progress: ProgressNames): void;
+};
+
+const initialState: NonFunctionProperties<State> = {
+  progress: "COVER_1",
+};
+export const store = create<State>((set, get) => {
+  const setWithLog: SetState<State> = (partial, replace) => {
+    console.log("Changed state with partial:");
+    console.table(partial);
+    set(partial, replace);
+  };
+  return {
+    ...initialState,
+    setProgress(progress: ProgressNames) {
+      setWithLog({ progress: progress });
+    },
+  };
+});
+
+export const useStore = createStoreHook(store);
+
+// Type helpers
+type CallbackFunctionVariadic = (...args: never[]) => void;
+type NonFunctionPropertyNames<T> = {
+  [K in keyof T]: T[K] extends CallbackFunctionVariadic ? never : K;
+}[keyof T];
+type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
