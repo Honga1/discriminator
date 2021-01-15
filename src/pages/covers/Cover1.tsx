9b995e94e64cd24a35117dfa962c619a691da9a0
Jae Perris <jae.perris@gmail.com>
2021-01-15 13:47:04 +0100

    Fix Cover1 draw boundaries

--- src/pages/covers/Cover1.tsx
+++ src/pages/covers/Cover1.tsx
@@ -12,22 +12,29 @@ export const Cover1 = () => {
     contextRef.current = maybeContext;
   }, []);
 
-  const paint = (pointerX: number, pointerY: number) => {
-    const context = contextRef.current;
-    if (!context) return;
+  useEffect(() => {
+    const paint = (pointerX: number, pointerY: number) => {
+      const context = contextRef.current;
+      if (!context) return;
 
-    requestAnimationFrame(() => {
-      for (let x = pointerX - 10; x < pointerX + 10; x++) {
-        for (let y = pointerY - 10; y < pointerY + 10; y++) {
-          context.fillStyle = `hsl(${x}, 100%, 50%)`;
-          context.beginPath();
+      requestAnimationFrame(() => {
+        for (let x = pointerX - 10; x < pointerX + 10; x++) {
+          for (let y = pointerY - 10; y < pointerY + 10; y++) {
+            context.fillStyle = `hsl(${x % 255}, 100%, 50%)`;
+            context.beginPath();
 
-          context.ellipse(x, y, 3, 3, 0, 0, 2 * Math.PI);
-          context.fill();
+            context.ellipse(x, y, 3, 3, 0, 0, 2 * Math.PI);
+            context.fill();
+          }
         }
-      }
+      });
+    };
+    window.addEventListener("mousemove", (event) => {
+      const x = event.clientX;
+      const y = event.clientY;
+      paint(x, y);
     });
-  };
+  });
 
   return (
     <canvas
@@ -35,11 +42,6 @@ export const Cover1 = () => {
       width={window.innerWidth}
       height={window.innerHeight}
       ref={canvasRef}
-      onMouseMove={(event) => {
-        const x = event.clientX;
-        const y = event.clientY;
-        paint(x, y);
-      }}
     ></canvas>
   );
 };
