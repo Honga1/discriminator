49e338584a0399f3c68ecd0ad5ba8967116217bc
Jae Perris <jae.perris@gmail.com>
2021-01-15 13:29:40 +0100

    Add dummy cover1

--- src/pages/covers/Cover1.tsx
+++ src/pages/covers/Cover1.tsx
@@ -1,5 +1,45 @@
-import { Text } from "grommet";
+import { useEffect, useRef } from "react";
 
 export const Cover1 = () => {
-  return <Text>Chapter 1 Cover</Text>;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const contextRef = useRef<CanvasRenderingContext2D>();
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const maybeContext = canvas.getContext("2d");
+    if (!maybeContext) return;
+    contextRef.current = maybeContext;
+  }, []);
+
+  const paint = (pointerX: number, pointerY: number) => {
+    const context = contextRef.current;
+    if (!context) return;
+
+    requestAnimationFrame(() => {
+      for (let x = pointerX - 10; x < pointerX + 10; x++) {
+        for (let y = pointerY - 10; y < pointerY + 10; y++) {
+          context.fillStyle = `hsl(${x}, 100%, 50%)`;
+          context.beginPath();
+
+          context.ellipse(x, y, 3, 3, 0, 0, 2 * Math.PI);
+          context.fill();
+        }
+      }
+    });
+  };
+
+  return (
+    <canvas
+      style={{ width: "100%", height: "100%" }}
+      width={window.innerWidth}
+      height={window.innerHeight}
+      ref={canvasRef}
+      onMouseMove={(event) => {
+        const x = event.clientX;
+        const y = event.clientY;
+        paint(x, y);
+      }}
+    ></canvas>
+  );
 };
