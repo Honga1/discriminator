7ef9ee3e4c8dbb77ea37193f258b0ab0b1a501e6
Jae Perris <jae.perris@gmail.com>
2021-02-17 12:05:11 +0100

    Remove unused import

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -1,15 +1,12 @@
 import { Grommet } from "grommet";
-import { deepMerge } from "grommet/utils";
 import React, { PropsWithChildren } from "react";
 import { ModalSelector } from "../pages/modals/ModalSelector";
 import { customTheme } from "../theme";
 import { NavMenu } from "./NavMenu";
 
-const mergedTheme = deepMerge(customTheme);
-
 export const PageContainer = ({ children }: PropsWithChildren<{}>) => {
   return (
-    <Grommet theme={mergedTheme} full background="yellow">
+    <Grommet theme={customTheme} full background="yellow">
       <NavMenu />
       {children}
       <ModalSelector />
