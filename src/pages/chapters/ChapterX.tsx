8ea5e332323705d5a88e23c2f3681789b57f3103
Jae Perris <jae.perris@gmail.com>
2021-02-18 16:55:33 +0100

    Make useChapter hook

--- src/pages/chapters/ChapterX.tsx
+++ src/pages/chapters/ChapterX.tsx
@@ -3,11 +3,23 @@ import { store } from "../../store/store";
 import videoSrc from "./../../720p.mp4";
 export const ChapterX = () => {
   const ref = useRef<HTMLVideoElement>(null);
-  const play = useCallback(() => ref.current?.play(), []);
-  const pause = useCallback(() => ref.current?.pause(), []);
+  useChapter(ref);
+
+  return (
+    <video
+      ref={ref}
+      style={{ boxSizing: "border-box", outline: "none" }}
+      width="100%"
+      src={videoSrc}
+    ></video>
+  );
+};
+function useChapter(ref: React.RefObject<HTMLVideoElement>) {
+  const play = useCallback(() => ref.current?.play(), [ref]);
+  const pause = useCallback(() => ref.current?.pause(), [ref]);
   const rewind = useCallback(() => {
     if (ref.current) ref.current.currentTime = 0;
-  }, []);
+  }, [ref]);
   const getIsPlaying = useCallback(() => {
     if (!ref.current) return false;
     return !!(
@@ -16,24 +28,35 @@ export const ChapterX = () => {
       !ref.current.ended &&
       ref.current.readyState > 2
     );
-  }, []);
+  }, [ref]);
 
   const getProgress = useCallback(() => {
     if (!ref.current) return 0;
     return ref.current.currentTime / ref.current.duration;
-  }, []);
+  }, [ref]);
 
-  const setProgress = useCallback((progress: number) => {
-    if (!ref.current) return;
-    ref.current.currentTime = progress * ref.current.duration;
-  }, []);
+  const setProgress = useCallback(
+    (progress: number) => {
+      if (!ref.current) return;
+      ref.current.currentTime = progress * ref.current.duration;
+    },
+    [ref]
+  );
 
-  const seekTimeDelta = useCallback((delta: number) => {
-    if (!ref.current) return;
-    const target = ref.current.currentTime + delta;
-    const clippedTarget = Math.min(Math.max(0, target), ref.current.duration);
-    ref.current.currentTime = clippedTarget;
-  }, []);
+  const seekTimeDelta = useCallback(
+    (delta: number) => {
+      if (!ref.current) return;
+      const target = ref.current.currentTime + delta;
+      const clippedTarget = Math.min(Math.max(0, target), ref.current.duration);
+      ref.current.currentTime = clippedTarget;
+    },
+    [ref]
+  );
+  const onClick = useCallback(() => (getIsPlaying() ? pause() : play()), [
+    getIsPlaying,
+    pause,
+    play,
+  ]);
 
   useEffect(() => {
     const updateStore = (
@@ -56,11 +79,14 @@ export const ChapterX = () => {
     };
 
     const video = ref.current;
+
+    ref.current?.addEventListener("click", onClick);
     ref.current?.addEventListener("playing", updateStore);
     ref.current?.addEventListener("pause", updateStore);
     ref.current?.addEventListener("timeupdate", updateStore);
     updateStore();
     return () => {
+      video?.removeEventListener("click", onClick);
       video?.removeEventListener("play", updateStore);
       video?.removeEventListener("pause", updateStore);
       video?.removeEventListener("timeupdate", updateStore);
@@ -74,15 +100,7 @@ export const ChapterX = () => {
     rewind,
     setProgress,
     seekTimeDelta,
+    ref,
+    onClick,
   ]);
-
-  return (
-    <video
-      ref={ref}
-      style={{ boxSizing: "border-box", outline: "none" }}
-      width="100%"
-      src={videoSrc}
-      onClick={() => (getIsPlaying() ? pause() : play())}
-    ></video>
-  );
-};
+}
