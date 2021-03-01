ba7b4bc5037ebd40be6a192a260795d5f1031bc7
Jae Perris <jae.perris@gmail.com>
2021-03-01 11:53:37 +0100

    Remove unused custom query button

--- src/pages/covers/Cover2.tsx
+++ src/pages/covers/Cover2.tsx
@@ -1,12 +1,10 @@
 import { Stack } from "grommet";
 import React from "react";
-import { WebcamPermissions } from "../../components/WebcamPermissions";
 
 export const Cover2 = () => {
   return (
     <>
       <Stack alignSelf="center" guidingChild={"last"}></Stack>
-      <WebcamPermissions />
     </>
   );
 };
