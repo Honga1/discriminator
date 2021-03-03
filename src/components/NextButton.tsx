2a1a1d21376da2f9b83b9d9fd3cf0576474063d8
Jae Perris <jae.perris@gmail.com>
2021-03-03 14:43:16 +0100

    Make next button clickable

--- src/components/NextButton.tsx
+++ src/components/NextButton.tsx
@@ -2,6 +2,7 @@ import { Box, Text } from "grommet";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { colorTheme } from "../theme";
+import { RoutedButton } from "./RoutedAnchor";
 
 const AnimatedBackground = styled(Box)<{ width: number }>`
   transition: all 1s linear;
@@ -13,8 +14,11 @@ const AnimatedBackground = styled(Box)<{ width: number }>`
   z-index: -1;
 `;
 
-const AnimateEverything = styled(Box)`
-  transition: all 1s linear;
+const AnimateWidthAndHover = styled(Box)`
+  transition: width 1s linear, transform 0.2s;
+  &:hover {
+    transform: translateY(-3px);
+  }
 `;
 
 export const NextButton = () => {
@@ -26,35 +30,41 @@ export const NextButton = () => {
   }, [progress]);
 
   return (
-    <AnimateEverything
-      border={{ size: "3px", color: colorTheme.white }}
-      style={{ zIndex: 10, position: "relative" }}
-      pad={{ horizontal: "21px", vertical: "5px" }}
-      width={progress < 2 ? "80px" : "286px"}
-    >
-      <AnimatedBackground
-        background="blue"
-        width={progress / 5}
-      ></AnimatedBackground>
-      <Box direction="row" align="center" gap="10px" overflow="hidden">
-        <Box flex={false} width="32px" height="32px">
-          <svg
-            width="32"
-            height="32"
-            viewBox="0 0 32 32"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect x="7" y="11" width="10" height="10" fill="#F8F9FA" />
-            <path d="M27 16L17 25L17 7L27 16Z" fill="#F8F9FA" />
-          </svg>
-        </Box>
-        <Box flex={false} height="24px" overflow="hidden">
-          <Text color="white" size="24px" style={{ lineHeight: "100%" }}>
-            Go to next chapter
-          </Text>
-        </Box>
-      </Box>
-    </AnimateEverything>
+    <RoutedButton
+      plain
+      href="/home"
+      label={
+        <AnimateWidthAndHover
+          border={{ size: "3px", color: colorTheme.white }}
+          style={{ zIndex: 10, position: "relative" }}
+          pad={{ horizontal: "21px", vertical: "5px" }}
+          width={progress < 2 ? "80px" : "286px"}
+        >
+          <AnimatedBackground
+            background="blue"
+            width={progress / 5}
+          ></AnimatedBackground>
+          <Box direction="row" align="center" gap="10px" overflow="hidden">
+            <Box flex={false} width="32px" height="32px">
+              <svg
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect x="7" y="11" width="10" height="10" fill="#F8F9FA" />
+                <path d="M27 16L17 25L17 7L27 16Z" fill="#F8F9FA" />
+              </svg>
+            </Box>
+            <Box flex={false} height="24px" overflow="hidden">
+              <Text color="white" size="24px" style={{ lineHeight: "100%" }}>
+                Go to next chapter
+              </Text>
+            </Box>
+          </Box>
+        </AnimateWidthAndHover>
+      }
+    ></RoutedButton>
   );
 };
