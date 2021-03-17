75ceba3c3f8da24050c53c6da25402775705db08
Jae Perris <jae.perris@gmail.com>
2021-03-17 13:04:35 +0100

    Allow import of .mp3

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -4,5 +4,9 @@ declare module "*.mp4" {
   const content: string;
   export default content;
 }
+declare module "*.mp3" {
+  const content: string;
+  export default content;
+}
 
 declare module "grommet/components/Stack/StyledStack";
