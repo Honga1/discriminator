6a57c6a7046366bd15e9fa90567332f089405543
Jae Perris <jae.perris@gmail.com>
2021-03-30 16:31:17 +0200

    Add talking bars animation

--- src/pages/chapters/chapter3/Chapter3.tsx
+++ src/pages/chapters/chapter3/Chapter3.tsx
@@ -1,5 +1,12 @@
 import { Box, ResponsiveContext } from "grommet";
-import React, { useContext, useRef, useState } from "react";
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { FinishedButton } from "../../../components/FinishedButton";
 import { useChapter } from "../../../hooks/useChapter";
 import { useIsActive } from "../../../hooks/useIsActive";
@@ -12,6 +19,168 @@ import { Part2Screen1 } from "./Part2Screen1";
 import { Part2Screen2 } from "./Part2Screen2";
 import { Part2Screen3 } from "./Part2Screen3";
 import { Part3Screen1Selector } from "./Part3Screen1";
+
+export default function Chapter3() {
+  const ref = useRef<HTMLAudioElement>(null);
+
+  const [getByteData, audio] = useMemo(() => {
+    const audio = document.createElement("audio");
+    audio.src = audioSrc;
+    const context = new AudioContext();
+    const source = context.createMediaElementSource(audio);
+    const analyser = context.createAnalyser();
+    source.connect(analyser);
+    analyser.connect(context.destination);
+    analyser.fftSize = 64;
+    const bufferLength = analyser.fftSize;
+    const dataArray = new Uint8Array(bufferLength);
+
+    audio.addEventListener("play", () => {
+      if (context.state === "suspended") context.resume();
+    });
+
+    return [
+      () => {
+        analyser.getByteTimeDomainData(dataArray);
+        return dataArray;
+      },
+      audio,
+    ];
+  }, []);
+
+  useEffect(() => {
+    (ref as any).current = audio;
+  }, [audio]);
+
+  useChapter(ref, false);
+  const [seconds, setSeconds] = useState(0);
+
+  const [allowAutoPause, setAllowAutoPause] = useState(false);
+  const [isAutoPaused, setIsAutoPaused] = useState(false);
+
+  const [part, setPart] = useState<
+    | "PART_1_SCREEN_1"
+    | "PART_1_SCREEN_2"
+    | "PART_2_SCREEN_1"
+    | "PART_2_SCREEN_2"
+    | "PART_2_SCREEN_3"
+    | "PART_3_SCREEN_1"
+  >("PART_1_SCREEN_1");
+
+  useEffect(() => {
+    const onTimeUpdate = ({ nativeEvent: event }: { nativeEvent: Event }) => {
+      const audio = event.target as IMediaElement;
+      const seconds = Math.round(audio.currentTime);
+      setSeconds(seconds);
+      if (seconds < 15) {
+        store.setState({ isHeadingShown: true });
+      } else {
+        store.setState({ isHeadingShown: false });
+      }
+
+      if (seconds < 30) {
+        setAllowAutoPause(true);
+        setPart("PART_1_SCREEN_1");
+        setIsAutoPaused(false);
+      } else if (seconds < 96) {
+        if (seconds >= 94) {
+          if (allowAutoPause && !isAutoPaused) {
+            ref.current?.pause();
+            setIsAutoPaused(true);
+            setAllowAutoPause(false);
+          }
+        } else {
+          setAllowAutoPause(true);
+          setIsAutoPaused(false);
+        }
+        setPart("PART_1_SCREEN_2");
+      } else if (seconds < 123) {
+        setAllowAutoPause(true);
+        setIsAutoPaused(false);
+        setPart("PART_2_SCREEN_1");
+      } else if (seconds < 168) {
+        setAllowAutoPause(true);
+        setIsAutoPaused(false);
+
+        setPart("PART_2_SCREEN_2");
+      } else if (seconds < 187) {
+        if (seconds >= 185) {
+          if (allowAutoPause && !isAutoPaused) {
+            ref.current?.pause();
+            setIsAutoPaused(true);
+            setAllowAutoPause(false);
+          }
+        } else {
+          setAllowAutoPause(true);
+          setIsAutoPaused(false);
+        }
+        setPart("PART_2_SCREEN_3");
+      } else {
+        setAllowAutoPause(true);
+        setIsAutoPaused(false);
+        setPart("PART_3_SCREEN_1");
+      }
+    };
+
+    audio.ontimeupdate = (event) => onTimeUpdate({ nativeEvent: event });
+    audio.onplay = () => {
+      return setIsAutoPaused(false);
+    };
+  }, [allowAutoPause, audio, isAutoPaused]);
+
+  return (
+    <>
+      <Box style={{ position: "relative" }} width="100%" height="100%">
+        <ContinueButton
+          play={() => ref.current?.play()}
+          isAutoPaused={isAutoPaused}
+        />
+
+        {part === "PART_1_SCREEN_1" && (
+          <Part1Screen1 getByteData={getByteData} />
+        )}
+        {part === "PART_1_SCREEN_2" && (
+          <Part1Screen2Selector seconds={seconds} />
+        )}
+        {part === "PART_2_SCREEN_1" && <Part2Screen1 />}
+        {part === "PART_2_SCREEN_2" && <Part2Screen2 />}
+        {part === "PART_2_SCREEN_3" && <Part2Screen3 />}
+        {part === "PART_3_SCREEN_1" && (
+          <Part3Screen1Selector seconds={seconds} />
+        )}
+      </Box>
+    </>
+  );
+}
+
+export const useAnimationFrame = (
+  frameRate: number,
+  callback: (deltaTime: number) => void
+): void => {
+  const requestRef = useRef<number>();
+  const previousTimeRef = useRef<number>();
+
+  const animate = useCallback(
+    (time: number) => {
+      if (previousTimeRef.current !== undefined) {
+        const deltaTime = time - previousTimeRef.current;
+        callback(deltaTime);
+      }
+      previousTimeRef.current = time;
+    },
+    [callback]
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      requestRef.current = requestAnimationFrame(animate);
+    }, 1000 / frameRate);
+    return () => {
+      requestRef.current && cancelAnimationFrame(requestRef.current);
+      interval && clearInterval(interval);
+    };
+  }, [animate, frameRate]);
+};
 
 function ContinueButton(props: { isAutoPaused: boolean; play?: () => void }) {
   const isSmall = useContext(ResponsiveContext) === "small";
@@ -35,106 +204,5 @@ function ContinueButton(props: { isAutoPaused: boolean; play?: () => void }) {
         />
       )}
     </Box>
-  );
-}
-
-export default function Chapter3() {
-  const ref = useRef<HTMLAudioElement>(null);
-  useChapter(ref, false);
-  const [seconds, setSeconds] = useState(0);
-
-  const [allowAutoPause, setAllowAutoPause] = useState(false);
-  const [isAutoPaused, setIsAutoPaused] = useState(false);
-
-  const [part, setPart] = useState<
-    | "PART_1_SCREEN_1"
-    | "PART_1_SCREEN_2"
-    | "PART_2_SCREEN_1"
-    | "PART_2_SCREEN_2"
-    | "PART_2_SCREEN_3"
-    | "PART_3_SCREEN_1"
-  >("PART_1_SCREEN_1");
-
-  const onTimeUpdate = ({ nativeEvent: event }: { nativeEvent: Event }) => {
-    const audio = event.target as IMediaElement;
-    const seconds = Math.round(audio.currentTime);
-    setSeconds(seconds);
-    if (seconds < 15) {
-      store.setState({ isHeadingShown: true });
-    } else {
-      store.setState({ isHeadingShown: false });
-    }
-
-    if (seconds < 30) {
-      setAllowAutoPause(true);
-      setPart("PART_1_SCREEN_1");
-      setIsAutoPaused(false);
-    } else if (seconds < 96) {
-      if (seconds >= 94) {
-        if (allowAutoPause && !isAutoPaused) {
-          ref.current?.pause();
-          setIsAutoPaused(true);
-          setAllowAutoPause(false);
-        }
-      } else {
-        setAllowAutoPause(true);
-        setIsAutoPaused(false);
-      }
-      setPart("PART_1_SCREEN_2");
-    } else if (seconds < 123) {
-      setAllowAutoPause(true);
-      setIsAutoPaused(false);
-      setPart("PART_2_SCREEN_1");
-    } else if (seconds < 168) {
-      setAllowAutoPause(true);
-      setIsAutoPaused(false);
-
-      setPart("PART_2_SCREEN_2");
-    } else if (seconds < 187) {
-      if (seconds >= 185) {
-        if (allowAutoPause && !isAutoPaused) {
-          ref.current?.pause();
-          setIsAutoPaused(true);
-          setAllowAutoPause(false);
-        }
-      } else {
-        setAllowAutoPause(true);
-        setIsAutoPaused(false);
-      }
-      setPart("PART_2_SCREEN_3");
-    } else {
-      setAllowAutoPause(true);
-      setIsAutoPaused(false);
-      setPart("PART_3_SCREEN_1");
-    }
-  };
-
-  return (
-    <>
-      <audio
-        ref={ref}
-        src={audioSrc}
-        onTimeUpdate={onTimeUpdate}
-        onPlay={() => setIsAutoPaused(false)}
-      />
-
-      <Box style={{ position: "relative" }} width="100%" height="100%">
-        <ContinueButton
-          play={() => ref.current?.play()}
-          isAutoPaused={isAutoPaused}
-        />
-
-        {part === "PART_1_SCREEN_1" && <Part1Screen1 />}
-        {part === "PART_1_SCREEN_2" && (
-          <Part1Screen2Selector seconds={seconds} />
-        )}
-        {part === "PART_2_SCREEN_1" && <Part2Screen1 />}
-        {part === "PART_2_SCREEN_2" && <Part2Screen2 />}
-        {part === "PART_2_SCREEN_3" && <Part2Screen3 />}
-        {part === "PART_3_SCREEN_1" && (
-          <Part3Screen1Selector seconds={seconds} />
-        )}
-      </Box>
-    </>
   );
 }
