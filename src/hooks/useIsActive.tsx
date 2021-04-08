48b18590eeac3b33280713e6a671cfe671050b60
Jae Perris <jae.perris@gmail.com>
2021-04-08 11:23:11 +0200

    Make useIsActive singleton monitoring

--- src/hooks/useIsActive.tsx
+++ src/hooks/useIsActive.tsx
@@ -1,27 +1,48 @@
 import { Timeout } from "grommet";
 import { useEffect, useState } from "react";
 
+const listeners = new Set<(state: boolean) => void>();
+const addListener = (setIsActive: (state: boolean) => void) => {
+  if (listeners.size === 0) {
+    install();
+  }
+  listeners.add(setIsActive);
+
+  return () => {
+    listeners.delete(setIsActive);
+    if (listeners.size === 0) {
+      uninstall();
+    }
+  };
+};
+
+const setIsActive = (state: boolean) => {
+  listeners.forEach((listener) => listener(state));
+};
+
+let timeout: Timeout | undefined;
+const onActivity = () => {
+  setIsActive(true);
+  if (timeout) clearTimeout(timeout);
+
+  timeout = (setTimeout(() => {
+    setIsActive(false);
+  }, 4000) as unknown) as number;
+};
+
+const install = () => {
+  window.addEventListener("mousemove", onActivity);
+  window.addEventListener("touchstart", onActivity);
+};
+
+const uninstall = () => {
+  timeout && clearTimeout(timeout);
+  window.removeEventListener("mousemove", onActivity);
+  window.removeEventListener("touchstart", onActivity);
+};
+
 export function useIsActive() {
   const [isActive, setIsActive] = useState(true);
-  useEffect(() => {
-    let timeout: Timeout | undefined;
-    const onActivity = () => {
-      setIsActive(true);
-      if (timeout) clearTimeout(timeout);
-
-      timeout = (setTimeout(() => {
-        setIsActive(false);
-      }, 4000) as unknown) as number;
-    };
-    window.addEventListener("mousemove", onActivity);
-    window.addEventListener("touchstart", onActivity);
-
-    return () => {
-      timeout && clearTimeout(timeout);
-      window.removeEventListener("mousemove", onActivity);
-      window.removeEventListener("touchstart", onActivity);
-    };
-  }, []);
-
+  useEffect(addListener(setIsActive), []);
   return isActive;
 }
