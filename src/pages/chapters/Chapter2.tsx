d8c382625138c8ceaacc364e21d4afb05b03c0e5
Jae Perris <jae.perris@gmail.com>
2021-04-14 18:49:43 +0200

    Add AR objects

--- src/pages/chapters/Chapter2.tsx
+++ src/pages/chapters/Chapter2.tsx
@@ -1,16 +1,16 @@
 import { AnnotatedPrediction } from "@tensorflow-models/face-landmarks-detection/dist/mediapipe-facemesh";
 import "@tensorflow/tfjs-backend-webgl";
 import { Box } from "grommet";
-import React, { PropsWithChildren, useEffect, useRef } from "react";
+import React, { PropsWithChildren, useRef } from "react";
 import { Canvas, useFrame, useThree } from "react-three-fiber";
-import { BufferAttribute, BufferGeometry, Mesh } from "three";
+import { Line } from "react-three-fiber/components";
+import { BufferAttribute, BufferGeometry, Mesh, Vector3 } from "three";
 import { useChapter } from "../../hooks/useChapter";
 import { useStore } from "../../store/store";
 import videoSrc from "./../../p2.mp4";
 import { maskMesh, TRIANGULATION } from "./chapter2/mask";
 import { usePredictions } from "./chapter2/usePredictions";
 import { useWebcamAndCanvas } from "./chapter2/useWebcamAndCanvas";
-import { useAnimationFrame } from "./chapter3/useAnimationFrame";
 
 export type V3 = [number, number, number];
 export type V2 = [number, number];
@@ -43,10 +43,7 @@ export default function Chapter2() {
 }
 
 function WebcamOverlay() {
-  const rectRef = useRef<Mesh>();
   const webcamRef = useRef<HTMLVideoElement>(null);
-  const geometry = useRef<BufferGeometry>(null);
-  const objectRef = useRef<Mesh>();
 
   const webcamStream = useStore((state) => state.webcamStream);
 
@@ -56,7 +53,45 @@ function WebcamOverlay() {
   );
   const predictions = usePredictions(webcamRef);
 
-  useAnimationFrame(60, async () => {
+  return (
+    <>
+      <video
+        style={{
+          position: "absolute",
+          top: 0,
+          boxSizing: "border-box",
+          outline: "none",
+          width: "100%",
+          height: "100%",
+        }}
+        ref={webcamRef}
+        // hidden
+      ></video>
+      <Canvas
+        style={{
+          position: "absolute",
+          width: canvasWidth + "px",
+          height: canvasHeight + "px",
+          top: "50%",
+          transform: "translateY(-50%)",
+        }}
+        orthographic={false}
+      >
+        <Scene predictions={predictions}></Scene>
+      </Canvas>
+    </>
+  );
+}
+
+const Scene = ({
+  predictions,
+}: {
+  predictions: { current: AnnotatedPrediction[] };
+}) => {
+  const rectRef = useRef<Mesh>();
+  const geometry = useRef<BufferGeometry>(null);
+
+  useFrame(() => {
     if (!rectRef.current) return;
 
     if (predictions.current?.length <= 0) return;
@@ -86,41 +121,18 @@ function WebcamOverlay() {
 
   return (
     <>
-      <video
-        style={{
-          position: "absolute",
-          top: 0,
-          boxSizing: "border-box",
-          outline: "none",
-          width: "100%",
-          height: "100%",
-        }}
-        ref={webcamRef}
-        // hidden
-      ></video>
-      <Canvas
-        style={{
-          position: "absolute",
-          width: canvasWidth + "px",
-          height: canvasHeight + "px",
-          top: "50%",
-          transform: "translateY(-50%)",
-        }}
-        orthographic={false}
-      >
-        <mesh ref={objectRef}>
-          <boxBufferGeometry />
-          <meshPhongMaterial color="#f3f3f3" wireframe />
-        </mesh>
-        <WorldOffset>
-          <MaskOnOwnFaceWithImage predictions={predictions} />
-          {/* <Dots geometry={geometry}></Dots> */}
-          <Rect rectRef={rectRef}></Rect>
-        </WorldOffset>
-      </Canvas>
+      {/* <mesh>
+        <boxBufferGeometry />
+        <meshPhongMaterial color="#f3f3f3" wireframe />
+      </mesh> */}
+      <WorldOffset>
+        <MaskOnOwnFaceWithImage predictions={predictions} />
+        <Dots geometry={geometry}></Dots>
+        <Rect rectRef={rectRef}></Rect>
+      </WorldOffset>
     </>
   );
-}
+};
 
 /**
  * Makes world scale 0-1, and position 0-1 for width and height
@@ -142,6 +154,8 @@ const MaskOnOwnFaceWithImage = (props: {
   predictions: { current: AnnotatedPrediction[] };
 }) => {
   const ref = useRef<Mesh>();
+  const line = useRef<THREE.Line>();
+  const linehelper = useRef<THREE.Line>();
   // const texture = new TextureLoader().load('');
   // texture.encoding = sRGBEncoding;
   // texture.flipY = false;
@@ -161,12 +175,83 @@ const MaskOnOwnFaceWithImage = (props: {
     });
     positions.needsUpdate = true;
     geometry.computeVertexNormals();
+
+    if (!line.current) return;
+
+    const a = new Vector3(mesh[151]![0]!, mesh[151]![1]!, -mesh[151]![2]!);
+    const b = new Vector3(mesh[285]![0]!, mesh[285]![1]!, -mesh[285]![2]!);
+    const c = new Vector3(mesh[55]![0]!, mesh[55]![1]!, -mesh[55]![2]!);
+
+    const side1 = new Vector3().subVectors(a, b);
+    const side2 = new Vector3().subVectors(a, c);
+    const facingOutFromHead = new Vector3()
+      .crossVectors(side2, side1)
+      .normalize();
+
+    const a2 = new Vector3(...mesh[4]!);
+    const b2 = new Vector3(...mesh[10]!);
+    const c2 = new Vector3(...mesh[152]!);
+    const side3 = new Vector3().subVectors(a2, b2);
+    const side4 = new Vector3().subVectors(a2, c2);
+    const facingSidewaysFromHead = new Vector3()
+      .crossVectors(side3, side4)
+      .normalize();
+
+    const up = new Vector3(...mesh[9]!)
+      .sub(new Vector3(...mesh[199]!))
+      .normalize();
+
+    line.current.position.set(...mesh[4]!).add(new Vector3(0, 0, 0));
+
+    const worldPosition = new Vector3();
+    line.current.getWorldPosition(worldPosition);
+    line.current.up.copy(up);
+    line.current.lookAt(facingOutFromHead.clone().negate().add(worldPosition));
+
+    const linePositions = linehelper.current?.geometry.getAttribute("position");
+    if (!linePositions) return;
+    linePositions.setXYZ(0, mesh[4]![0], mesh[4]![1], mesh[4]![2]!);
+    // linePositions.setXYZ(1, mesh[4]![0], mesh[4]![1], mesh[4]![2]!);
+    linePositions.setXYZ(1, ...new Vector3(...mesh[4]!).add(up).toArray());
+    linePositions.setXYZ(2, mesh[4]![0], mesh[4]![1], mesh[4]![2]!);
+    linePositions.setXYZ(
+      3,
+      ...new Vector3(...mesh[4]!).add(facingOutFromHead).toArray()
+    );
+    linePositions.setXYZ(4, mesh[4]![0], mesh[4]![1], mesh[4]![2]!);
+    linePositions.setXYZ(
+      5,
+      ...new Vector3(...mesh[4]!).add(facingSidewaysFromHead).toArray()
+    );
+    linePositions.setXYZ(6, mesh[4]![0], mesh[4]![1], mesh[4]![2]!);
+
+    linePositions.needsUpdate = true;
   });
 
   return (
-    <mesh ref={ref} geometry={maskMesh.geometry}>
-      <meshNormalMaterial toneMapped={false} />
-    </mesh>
+    <>
+      <Line
+        ref={linehelper}
+        geometry={new BufferGeometry().setFromPoints(
+          Array.from({ length: 7 }).map(() => new Vector3())
+        )}
+      >
+        <lineBasicMaterial color="red"></lineBasicMaterial>
+      </Line>
+      <group ref={line} frustumCulled={false} scale={[0.04, 0.1, 0.1]}>
+        <mesh position={[0, 0, -1.5]}>
+          <boxBufferGeometry></boxBufferGeometry>
+          <meshNormalMaterial
+            color="red"
+            transparent
+            opacity={1}
+          ></meshNormalMaterial>
+        </mesh>
+      </group>
+      <mesh ref={ref} geometry={maskMesh.geometry}>
+        <meshNormalMaterial toneMapped={false} transparent opacity={0.4} />
+      </mesh>
+    </>
   );
 };
 
