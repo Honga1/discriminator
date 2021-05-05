decf7e92b81106c3547dff6860407cbf81830fa1
Jae Perris <jae.perris@gmail.com>
2021-05-05 13:21:08 +0200

    Remove chevron down

--- src/chapters/chapter3/Part2Screen3/ScrollBanner.tsx
+++ src/chapters/chapter3/Part2Screen3/ScrollBanner.tsx
@@ -18,33 +18,8 @@ export const ScrollBanner = memo(({ isShown }: { isShown: boolean }) => {
       align="center"
       justify="center"
       isShown={isShown}
-    >
-      <ChevronDown />
-    </FadeOutBox>
+    ></FadeOutBox>
   );
 });
 
 ScrollBanner.displayName = "ScrollBanner";
-
-const ChevronDown = memo(() => {
-  return (
-    <Box width="80px" height="80px" flex={false}>
-      <svg
-        width="100%"
-        height="100%"
-        viewBox="0 0 80 81"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M22.4739 25.5L15.0005 32.9734L39.912 57.8849L39.9999 57.797L40.0879 57.885L64.9994 32.9735L57.5259 25.5L39.9999 43.026L22.4739 25.5Z"
-          fill="#20BF00"
-        />
-      </svg>
-    </Box>
-  );
-});
-
-ChevronDown.displayName = "ChevronDown";
