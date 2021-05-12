f91e4ebb5f5000e531beab2f1bc5923f31650852
Jae Perris <jae.perris@gmail.com>
2021-05-12 18:03:12 +0200

    Add eyes cover

--- src/chapters/chapter2/Cover2.tsx
+++ src/chapters/chapter2/Cover2.tsx
@@ -1,28 +1,82 @@
-import React, { useContext, useEffect } from "react";
-import { store, useStore } from "../../store/store";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { useAnimationFrame } from "src/hooks/useAnimationFrame";
+import { useAsyncMemo } from "src/hooks/useAsyncMemo";
+import { store } from "../../store/store";
+import { eyes } from "./eyes/eyes";
 import { SceneContext } from "./part1/SceneContext";
 
 export default function Cover2() {
+  const ref = useRef<HTMLCanvasElement>(null);
   const predictions = useContext(SceneContext).facemesh;
-  const isFirstPredictionComplete = useStore(
-    (state) => state.isFirstPredictionComplete
+
+  const [openCount, setOpenCount] = useState(0);
+  const wasOpenLastFrame = useRef(false);
+
+  const images = useAsyncMemo(
+    async () =>
+      Promise.all(
+        eyes.map(
+          async (src) =>
+            new Promise<HTMLImageElement>((resolve, reject) => {
+              const image = new Image();
+              image.onload = () => resolve(image);
+              image.onerror = () => reject();
+              image.src = src;
+            })
+        )
+      ),
+    [],
+    []
   );
   useEffect(() => {
     store.setState({ isCameraEnabled: true });
   }, []);
 
+  useAnimationFrame(10, () => {
+    const isOpen = predictions.current[0]?.eyesOpened ?? false;
+
+    if (isOpen && !wasOpenLastFrame.current) {
+      setOpenCount((count) => count + 1);
+    }
+
+    if (isOpen) {
+      wasOpenLastFrame.current = true;
+    } else {
+      wasOpenLastFrame.current = false;
+    }
+  });
+
+  useEffect(() => {
+    if (!ref.current) return;
+    const context = ref.current?.getContext("2d");
+    if (!context) return;
+    if (images.length === 0) return;
+    const image = images[Math.floor(Math.random() * images.length)]!;
+
+    const x = Math.random() * ref.current.width;
+    const y = Math.random() * ref.current.height;
+    const width = Math.random() * 75 + 25;
+    const height = (width * image.naturalHeight) / image.naturalWidth;
+    context.drawImage(image, x - width / 2, y - height / 2, width, height);
+  }, [images, openCount]);
+
   return (
     <div
       style={{
         position: "absolute",
-        // width: "100%",
-        // height: "100%",
+        width: "100%",
+        height: "100%",
         top: "50%",
         transform: "translate(-50%, -50%)",
         left: "50%",
       }}
     >
-      {!isFirstPredictionComplete ? "LOADING" : "COMPLETE"}
+      <canvas
+        width={window.innerWidth / 2}
+        height={window.innerHeight / 2}
+        style={{ width: "100%", height: "100%" }}
+        ref={ref}
+      ></canvas>
     </div>
   );
 }
---
+++ src/chapters/chapter2/eyes/eyes.tsx
@@ -0,0 +1,89 @@
+import image0001 from "./0001.jpg";
+import image0002 from "./0002.jpg";
+import image0003 from "./0003.jpg";
+import image0004 from "./0004.jpg";
+import image0005 from "./0005.jpg";
+import image0006 from "./0006.jpg";
+import image0007 from "./0007.jpg";
+import image0008 from "./0008.jpg";
+import image0009 from "./0009.jpg";
+import image0010 from "./0010.jpg";
+import image0011 from "./0011.jpg";
+import image0012 from "./0012.jpg";
+import image0013 from "./0013.jpg";
+import image0014 from "./0014.jpg";
+import image0015 from "./0015.jpg";
+import image0016 from "./0016.jpg";
+import image0017 from "./0017.jpg";
+import image0018 from "./0018.jpg";
+import image0019 from "./0019.jpg";
+import image0020 from "./0020.jpg";
+import image0021 from "./0021.jpg";
+import image0022 from "./0022.jpg";
+import image0023 from "./0023.jpg";
+import image0024 from "./0024.jpg";
+import image0025 from "./0025.jpg";
+import image0026 from "./0026.jpg";
+import image0027 from "./0027.jpg";
+import image0028 from "./0028.jpg";
+import image0029 from "./0029.jpg";
+import image0030 from "./0030.jpg";
+import image0031 from "./0031.jpg";
+import image0032 from "./0032.jpg";
+import image0033 from "./0033.jpg";
+import image0034 from "./0034.jpg";
+import image0035 from "./0035.jpg";
+import image0036 from "./0036.jpg";
+import image0037 from "./0037.jpg";
+import image0038 from "./0038.jpg";
+import image0039 from "./0039.jpg";
+import image0040 from "./0040.jpg";
+import image0041 from "./0041.jpg";
+import image0042 from "./0042.jpg";
+import image0043 from "./0043.jpg";
+
+export const eyes = [
+  image0001,
+  image0002,
+  image0003,
+  image0004,
+  image0005,
+  image0006,
+  image0007,
+  image0008,
+  image0009,
+  image0010,
+  image0011,
+  image0012,
+  image0013,
+  image0014,
+  image0015,
+  image0016,
+  image0017,
+  image0018,
+  image0019,
+  image0020,
+  image0021,
+  image0022,
+  image0023,
+  image0024,
+  image0025,
+  image0026,
+  image0027,
+  image0028,
+  image0029,
+  image0030,
+  image0031,
+  image0032,
+  image0033,
+  image0034,
+  image0035,
+  image0036,
+  image0037,
+  image0038,
+  image0039,
+  image0040,
+  image0041,
+  image0042,
+  image0043,
+];
