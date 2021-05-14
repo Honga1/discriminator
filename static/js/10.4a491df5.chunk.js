dfe2c34caa3c5aba2a6bad60c8e93b7546f45c73
Jae Perris <jae.perris@gmail.com>
2021-05-14 12:16:41 +0200

    Updates

--- static/js/10.4a491df5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpdisc=this.webpackJsonpdisc||[]).push([[10],{414:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return c}));var s=n(1),i=n(4),r=n(0);function c(){var t=Object(i.b)((function(t){return t.isFirstPredictionComplete}));return Object(s.useEffect)((function(){i.a.setState({isCameraEnabled:!0})}),[]),Object(r.jsx)("div",{style:{position:"absolute",top:"50%",transform:"translate(-50%, -50%)",left:"50%"},children:t?"COMPLETE":"LOADING"})}}}]);
-//# sourceMappingURL=10.4a491df5.chunk.js.map
